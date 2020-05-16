6fe6cc3df32767c86822066a701059b95e67177c
Muhammad Athar <muhammad.athar@agoda.com>
2020-05-17 02:04:17 +0700

    site updated

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-const path = require("path")
-const fs = require("fs")
-
-exports.onPreInit = () => {
-  if (process.argv[2] === "build") {
-    fs.unlink(path.join(__dirname, "docs"), () => { console.log("deleted docs") })
-  }
-}
